be687d3351bfac242e89929828fe134783aa981f
Irina <irinavasilitav@gmail.com>
2022-10-27 11:53:51 +0200

    Add existing project files to Git

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './TrackList.css';
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+    render() {
+        const tracks = this.props.tracks;
+
+        return(
+            <div className="TrackList">
+                {
+                    tracks && tracks.map(track => {
+                        return <Track track={track} 
+                                    key={track.id}
+                                    onAdd={this.props.onAdd}
+                                    onRemove={this.props.onRemove}
+                                    isRemoval={this.props.isRemoval}/>
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default TrackList;
